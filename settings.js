718c9049cc179689a526f49fda355fd9bf78946d
sakariye475 <xamsec419@gmail.com>
2026-02-02 21:41:39 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 const settings = {
   packname: 'Knight Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "ΘᗰᗩᑎIᗴA",
+  botOwner: 'ΘᗰᗩᑎIᗴA', // Your name
+  ownerNumber: '2520906417995', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "private",
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
